d990a234391401477d82a4fb375456ac303959bf
winters <winters0727@gmail.com>
2024-02-01 01:40:32 +0900

    chore: cors 적용

--- app.ts
+++ app.ts
@@ -1,36 +1,42 @@
-import express from 'express';
-import session from 'express-session';
-import bodyParser from 'body-parser';
+import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-import './env.ts';
-import { initializeConnection } from './database.ts';
+import "./env.ts";
+import { initializeConnection } from "./database.ts";
 
-import ApiRouter from './src/routes/api.ts';
+import ApiRouter from "./src/routes/api.ts";
 
 const app = express();
 
-app.set('trust proxy', true);
+app.set("trust proxy", true);
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || 'winters',
+    secret: process.env.SESSION_SECRET || "winters",
     resave: false,
     saveUninitialized: true,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
-      secure: process.env.NODE_ENV === 'production' ? true : false,
+      secure: process.env.NODE_ENV === "production" ? true : false,
     },
-  }),
+  })
 );
 
 app.use(bodyParser.json());
 
-app.use('/api', ApiRouter);
+app.use("/api", ApiRouter);
 
 app.listen(
   process.env.PORT ? parseInt(process.env.PORT) : 3000,
-  '0.0.0.0',
+  "0.0.0.0",
   () => {
     initializeConnection();
-    console.log('App is running...');
-  },
+    console.log("App is running...");
+  }
 );
