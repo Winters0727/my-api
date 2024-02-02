5aafb1dd00b02fbfdfd3e14992f3d49a29e269b7
winters <winters0727@gmail.com>
2024-02-02 15:55:01 +0900

    feat: 캐릭터별 카드 데이터 타입별 정렬 기능 추가

--- src/controllers/furuyoni/card.controller.ts
+++ src/controllers/furuyoni/card.controller.ts
@@ -1,11 +1,29 @@
 import type { Request, Response } from "express";
+import type { Document } from "mongodb";
 
 import { getCollection } from "../../../database.js";
 
 import type { Language } from "@customTypes/furuyoni/index.type";
 import type { CharacterMode } from "@customTypes/furuyoni/character.type";
+import type { Card } from "@customTypes/furuyoni/card.type";
 
 const DEFAULT_LANG = "kor";
+
+const sortCardsByType = ({
+  cards,
+  normalCards,
+  specialCards,
+  extraCards,
+}: {
+  cards: Card[];
+  normalCards: string[];
+  specialCards: string[];
+  extraCards: string[];
+}) => ({
+  normalCards: cards.filter((card) => normalCards.includes(card.code)),
+  specialCards: cards.filter((card) => specialCards.includes(card.code)),
+  extraCards: cards.filter((card) => extraCards.includes(card.code)),
+});
 
 const getCardByCode = async (req: Request, res: Response) => {
   try {
@@ -93,27 +111,78 @@ const getCardsByCharName = async (req: Request, res: Response) => {
 
     const cardCollection = getCollection("furuyoni", "card");
 
-    if (mode) {
-      const characterCollection = getCollection("furuyoni", "character");
-      const charData = await characterCollection.findOne({
-        "engData.name.O": { $eq: charName },
-      });
+    const characterCollection = getCollection("furuyoni", "character");
+    const charData = await characterCollection.findOne({
+      $or: [
+        {
+          "korData.name.O": {
+            $eq: charName,
+          },
+        },
+        {
+          "engData.name.O": {
+            $eq: charName.toLowerCase(),
+          },
+        },
+        {
+          "jpnData.name.O": {
+            $eq: charName,
+          },
+        },
+      ],
+    });
 
-      const modeUpperCase = mode.toUpperCase();
+    if (charData) {
+      if (mode) {
+        const modeUpperCase = mode.toUpperCase();
 
-      if (charData && charData.mode.includes(modeUpperCase)) {
-        const modeNormalCards = charData.normalCards[modeUpperCase];
-        const modeSpecialCards = charData.specialCards[modeUpperCase];
+        if (charData && charData.mode.includes(modeUpperCase)) {
+          const normalCards = charData.normalCards[modeUpperCase];
+          const specialCards = charData.specialCards[modeUpperCase];
+          const extraCards = charData.extraCards[modeUpperCase];
 
+          const cards = await cardCollection
+            .find({
+              charName: { $eq: charData.engData.name.O },
+              $or: [
+                { code: { $in: normalCards } },
+                { code: { $in: specialCards } },
+                { code: { $in: extraCards } },
+              ],
+            })
+            .project<Card>({
+              _id: 0,
+              fullCode: 1,
+              code: 1,
+              charName: 1,
+              data: `$${(lang && lang.toLowerCase()) || DEFAULT_LANG}Data`,
+              relatedExtraCards: 1,
+              revisionCount1: 1,
+              distance: 1,
+              shieldDamage: 1,
+              hpDamage: 1,
+              deployCount: 1,
+              cost: 1,
+            })
+            .toArray();
+
+          if (cards)
+            return res.status(200).json({
+              result: "success",
+              cards: sortCardsByType({
+                cards,
+                normalCards,
+                specialCards,
+                extraCards,
+              }),
+            });
+        }
+      } else {
         const cards = await cardCollection
           .find({
-            charName: { $eq: charName },
-            $or: [
-              { code: { $in: modeNormalCards } },
-              { code: { $in: modeSpecialCards } },
-            ],
+            charName: { $eq: charData.engData.name.O },
           })
-          .project({
+          .project<Card>({
             _id: 0,
             fullCode: 1,
             code: 1,
@@ -129,39 +198,31 @@ const getCardsByCharName = async (req: Request, res: Response) => {
           })
           .toArray();
 
+        const modes = charData.mode;
+
+        const normalCards = modes
+          .map((mode: CharacterMode) => charData.normalCards[mode])
+          .flat();
+        const specialCards = modes
+          .map((mode: CharacterMode) => charData.specialCards[mode])
+          .flat();
+        const extraCards = modes
+          .map((mode: CharacterMode) => charData.extraCards[mode])
+          .flat();
+
         if (cards)
           return res.status(200).json({
             result: "success",
-            cards,
+            cards: sortCardsByType({
+              cards,
+              normalCards,
+              specialCards,
+              extraCards,
+            }),
           });
       }
-    } else {
-      const cards = await cardCollection
-        .find({
-          charName: { $eq: charName },
-        })
-        .project({
-          _id: 0,
-          fullCode: 1,
-          code: 1,
-          charName: 1,
-          data: `$${(lang && lang.toLowerCase()) || DEFAULT_LANG}Data`,
-          relatedExtraCards: 1,
-          revisionCount1: 1,
-          distance: 1,
-          shieldDamage: 1,
-          hpDamage: 1,
-          deployCount: 1,
-          cost: 1,
-        })
-        .toArray();
-
-      if (cards)
-        return res.status(200).json({
-          result: "success",
-          cards,
-        });
     }
+
     return res.status(404).json({
       result: "fail",
       error: "Not found",
