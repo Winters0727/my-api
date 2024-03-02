dd2ca84eaa5ad71bbc3b14478c3651eed5e18146
winters <winters0727@gmail.com>
2024-03-02 15:58:12 +0900

    refactor: API cors 설정 수정

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,15 +6,13 @@ import FuruyoniRouter from "./furuyoni/index.js";
 
 const router = express.Router();
 
+router.use("/blog", BlogRouter);
 router.use(
-  "/blog",
+  "/furuyoni",
   cors({
-    origin: "http://localhost:3000",
     methods: ["GET"],
-    credentials: true,
   }),
-  BlogRouter
+  FuruyoniRouter
 );
-router.use("/furuyoni", cors(), FuruyoniRouter);
 
 export default router;
--- src/routes/blog/index.ts
+++ src/routes/blog/index.ts
@@ -1,11 +1,26 @@
 import express from "express";
+import cors from "cors";
 
 import { getVisitData } from "../../controllers/blog/index.controller.js";
 import { getGameData } from "../../controllers/blog/game.controller.js";
 
 const router = express.Router();
 
-router.get("/visit", getVisitData);
-router.get("/game/:id", getGameData);
+router.get(
+  "/visit",
+  cors({
+    methods: ["GET"],
+  }),
+  getVisitData
+);
+router.get(
+  "/game/:id",
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET"],
+    credentials: true,
+  }),
+  getGameData
+);
 
 export default router;
